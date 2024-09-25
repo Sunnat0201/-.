bbbda5592086558bc2bfb5a1866e82e679944c11
HappY Millionaire <sunnat.fakhreev.02@gmail.com>
2024-09-26 01:58:03 +0300

    Попытка №1

---
+++ main.py
@@ -0,0 +1,9 @@
+#my_string = input("Добрый день. Чем могу вам помочь? ")
+#print = len( my_string)
+
+name = 'Добрый день. Я барбоскин'
+print (name. upper())
+print(name.lower())
+print(name.replace(' ', ''))
+print (name [0])
+print(name[-1])
